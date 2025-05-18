7622b6c8e684d4885229a059f4de0d19e5be8a27
Sinokholo <222705698@mycput.ac.za>
2025-05-18 21:07:09 +0200

    Added DrivingTestFactory

---
+++ src/main/java/za/ac/cput/Factory/DrivingTestFactory.java
@@ -0,0 +1,44 @@
+package za.ac.cput.Factory;
+
+import za.ac.cput.Domain.bookings.DrivingTest;
+import za.ac.cput.Util.Helper;
+
+import java.time.LocalDate;
+
+public class DrivingTestFactory {
+    public static DrivingTest createDrivingTest(int driverLicenseID, String examiner, int testID, String testAddress, String testVenue,
+                                                LocalDate testDate, boolean testResult, double testAmount) {
+        if (Helper.isNullOrEmpty(examiner) || Helper.isNullOrEmpty(testAddress)
+                || Helper.isNullOrEmpty(testVenue) ) {
+            return null;
+        }
+
+
+        if (testDate.isBefore(LocalDate.now())) {
+            return null;
+        }
+
+        DrivingTest drivingTest = new DrivingTest.Builder()
+                .setDriverLicenseID(driverLicenseID)
+                .setExaminer(examiner)
+                .setTestID(testID)
+                .setTestAddress(testAddress)
+                .setTestVenue(testVenue)
+                .setTestDate(testDate)
+                .setTestResult(testResult)
+                //.setLicenseCode(licenseCode)
+                .setTestAmount(testAmount)
+
+                .build();
+
+        return drivingTest;
+    }
+
+        }
+
+
+
+
+
+
+
