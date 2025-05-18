2956d14cb36dc52ea8560f6c938c438b34009d66
Sinokholo <222705698@mycput.ac.za>
2025-05-18 20:59:49 +0200

    Added LearnersTestFactory

---
+++ src/main/java/za/ac/cput/Factory/LearnersTestFactory.java
@@ -0,0 +1,26 @@
+package za.ac.cput.Factory;
+
+import za.ac.cput.Domain.bookings.LearnersTest;
+import za.ac.cput.Util.Helper;
+
+import java.time.LocalDate;
+
+public class LearnersTestFactory {
+    // Factory method to create a LearnersTest object
+    public static LearnersTest createLearnersTest(int learnersLicenseID, String testAddress, String testVenue,
+                                                  LocalDate testDate, boolean testResult, String licenseCode,
+                                                  double testAmount) {
+        if (Helper.isNullOrEmpty(testAddress) || Helper.isNullOrEmpty(testVenue) || Helper.isNullOrEmpty(licenseCode)) {
+            return null;
+        }
+        return new LearnersTest.Builder()
+                .setLearnersLicenseID(learnersLicenseID)
+                .setTestAddress(testAddress)
+                .setTestVenue(testVenue)
+                .setTestDate(testDate)
+                .setTestResult(testResult)
+                .setLicenseCode(licenseCode)
+                .setTestAmount(testAmount)
+                .build();
+    }
+}
