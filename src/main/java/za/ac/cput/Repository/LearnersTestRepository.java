1c3ab11dd746a2d197abf4b977385abf7bf5e0f6
Sinokholo <222705698@mycput.ac.za>
2025-05-25 19:39:40 +0200

    Added LearnersTestRepository.java

---
+++ src/main/java/za/ac/cput/Repository/LearnersTestRepository.java
@@ -0,0 +1,7 @@
+package za.ac.cput.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.Domain.bookings.LearnersTest;
+
+public interface LearnersTestRepository extends JpaRepository<LearnersTest,Integer> {
+}
